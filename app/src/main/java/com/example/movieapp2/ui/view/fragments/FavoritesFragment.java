2f6d863352ba43b06f3c477b5f638a27d0ea4df6
ayman_rahmon <ayman_rahmon@hotmail.com>
2019-08-29 13:53:36 +0300

    adding the tabs layout structure

---
+++ app/src/main/java/com/example/movieapp2/ui/view/fragments/FavoritesFragment.java
@@ -0,0 +1,23 @@
+package com.example.movieapp2.ui.view;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class FavoritesFragment extends Fragment {
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/movieapp2/ui/view/fragments/adapters/TabAdapter.java
@@ -0,0 +1,4 @@
+package com.example.movieapp2.ui.view.fragments.adapters;
+
+public class TabAdapter {
+}
